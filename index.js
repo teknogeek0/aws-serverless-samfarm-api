f541d0d1ec28161fa19e9b1d59234e5f1c5dc992
Chris Munns <munns@amazon.com>
2018-08-01 08:19:48 -0700

    update sam dance node.js version

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ exports.handler = (event, context, callback) => {
 
     callback(null, {
         "statusCode": 200,
-        "body": totalSAMs,
+        "body":  ""+totalSAMs,
         "headers":
         {
             "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
